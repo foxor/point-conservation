2fc334fb979c126bc44db085dc825753b82fcc4f
root <root@Odin.(none)>
2011-09-13 04:49:04 +0000

    lolapps2 tournament

--- converter.py
+++ converter.py
@@ -3,23 +3,26 @@
 # files contain:
 # name1 name2 score1 score2
 
-import math
-
-ITERATIONS = 100
+ITERATIONS = 20
 START_SCORE = 5
 
-def score(rankings, name, season):
+def score(rankings, name):
   if not name in rankings:
     rankings[name] = START_SCORE
     return START_SCORE
   return rankings[name]
 
 def process_match(rankings, match, match_number, season):
-  pool = sum([score(rankings, x, season) for x in match[:2]])
-  actual_score = [float(x) for x in match[2:]]
-  difference = actual_score[0] - actual_score[1]
+  names = match[:2]
+  player_rankings = [score(rankings, x) for x in names]
+  pool = sum(player_rankings)
+  match_scores = [float(x) for x in match[2:]]
+  difference = match_scores[0] - match_scores[1]
   new_scores = [pool / 2 + difference / 2, pool / 2 - difference / 2]
-  #import pdb;pdb.set_trace()
+  if any([match_scores[x] == 0 and player_rankings[x] < new_scores[x] for x in [0, 1]]):
+    #print "Winner protection engaged for match: %s" % match
+    new_scores = player_rankings
+    
   for n,name in enumerate(match[:2]):
     season[name] = season.get(name, {})
     season[name][match_number] = new_scores[n]
@@ -27,11 +30,17 @@ def process_match(rankings, match, match_number, season):
 def process_file(fptr):
   matches = [x.strip().split(' ') for x in fptr]
   rankings = {}
-  for r in xrange(ITERATIONS):
+  _continue = True
+  while _continue:
+    _continue = False
     season = {}
     for n,m in list(enumerate(matches))[::1]:
       process_match(rankings, m, n, season)
-    #import pdb;pdb.set_trace()
+    for player in season:
+      new_rank = sum(season[player].values()) / len(season[player])
+      _continue |= abs(score(rankings, player) - new_rank) > 0.0005
+      rankings[player] = new_rank
+
     rankings = dict([(x, sum(season[x].values()) / len(season[x])) for x in season])
   return rankings
 
@@ -40,11 +49,15 @@ def print_rankings(rankings):
     print "<p>%d: %s with %.2f points</p>" %(n+1, r[0], r[1])
 
 if __name__ == '__main__':
-  print "trying to open file...<br/>"
+  #print "trying to open file...<br/>"
+  import sys
   try:
-    import sys
     fptr = open(sys.argv[1], 'r')
-    print_rankings(process_file(fptr))
+    rankings = process_file(fptr)
+    if rankings:
+      print_rankings(rankings)
+    else:
+      print "<p>Nobody has played yet</p>"
   except Exception, e:
     import sys
     print sys.exc_info()
--- submit.php
+++ submit.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
-<title>Isaac James.com</title> 
-<link type="text/css" rel="stylesheet" href="/static/menu.css" /> 
-<link type="text/css" rel="stylesheet" href="/static/base.css" /> 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script> 
- 
- 
-</head> 
-<body> 
-<ul id="nav"> 
-  <li class="current"><a href="/">Isaac James</a></li> 
-  <li><a href="#">Projects</a> 
-    <ul> 
-      <li><a href="/baseballbuddy">Baseball Buddy</a></li> 
-      <li><a href="/mandelbrot">Mandelbrot</a></li> 
-      <li><a href="http://puzzle-blackout.appspot.com/">Puzzle-blackout</a> 
-        <ul> 
-          <li><p>&nbsp;&nbsp;(click around, don't give up!)</p></li> 
-        </ul> 
-      </li> 
-      <li><a href="/point_conservation">Point Conservation Ranking System</a></li>
-    </ul> 
-  </li> 
-  <li><a href="/resume.pdf">Resume</a></li> 
-  <li><a href="/contact">Info</a></li> 
-</ul> 
-<div id="content">
-<h1>Rankings:</h1>
 <?php
 $fname = "input/".$_GET['tournament'];
 if (file_exists($fname)) {
@@ -35,21 +5,16 @@ if (file_exists($fname)) {
   $fptr = fopen($fname, 'a');
 } else {
   //echo "starting new tournament at ".$fname."...<br/>";
+  system('touch '.$fname);
   $fptr = fopen($fname, 'a');
   fwrite($fptr, "2\n");
 }
 echo "writing line for result...<br/>";
 if (!fwrite($fptr, $_GET['name1']." ".$_GET['name2']." ".$_GET["score1"]." ".$_GET['score2']."\n")) {
-  //echo "write failed<br/>";
+  echo "write failed".$fname."<br/>";
 }
 fclose($fptr);
 //echo "shelling out to converter.py for new rankings...<br/>";
 system('./converter.py input/'.$_GET['tournament']);
+header( 'Location: /point_conservation');
 ?>
-<h2>Thanks!</h2>
-</div> 
-<!-- credit where credit is due --!> 
-<div class="clear"></div> 
-<p class="footer"><a href="http://www.webdesignerwall.com/tutorials/css3-dropdown-menu/">CSS3 Dropdown Menu</a> <em>by</em> <a href="http://www.webdesignerwall.com">Web Designer Wall</a></p> 
-</body> 
-</html>
